34dccd3f07baf3224ac6651ff1c7f844a88a8837
dimon <patunutap@gmail.com>
2021-03-08 20:00:00 +0300

    feat: Добавлена возможность получения результата работы RQ Worker fix: Исправлены ошибки

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ Dmitry Livanov, 2021
 ver 0.0.2
 """
 from flask import Flask, request, abort, jsonify
+from rq.job import Job
+
 import config
 from task import get_hosts
 from rq import Queue
@@ -23,10 +25,19 @@ def get_task():
     if not request.json or not 'host' in request.json:
         abort(400)
     data = request.json
-    q.enqueue_call(
+    job = q.enqueue_call(
         func=get_hosts, args=(data['host'],), result_ttl=500
     )
-    return jsonify({"result": "OK"})
+    return jsonify({"job": job.id})
+
+
+@app.route("/api/v1/discovery/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+    job = Job.fetch(job_key, connection=conn)
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "waiting...", 202
 
 
 if __name__ == '__main__':
